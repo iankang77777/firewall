6e2545fd62fdec50cf647230ffaf51769302e0b2
PenguinMyangdoo <166986454+PenguinMyangdoo@users.noreply.github.com>
2025-08-31 15:01:59 +0900

    Added final commit

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
+import subprocess
+import sys
+
+
+def start_app():
+    return subprocess.Popen(
+        [sys.executable, "src/app.py"], creationflags=subprocess.CREATE_NEW_CONSOLE
+    )
+
+
+def start_launcher(app_pid):
+    return subprocess.Popen(
+        [sys.executable, "launcher.py", str(app_pid)],
+        creationflags=subprocess.CREATE_NO_WINDOW,
+    )
+
+
 def main():
-    print("Hello from firewall!")
+    app_proc = start_app()
+    print(f"App started with PID {app_proc.pid}")
+
+    launcher_proc = start_launcher(app_proc.pid)
+    print(f"Launcher started with PID {launcher_proc.pid}")
+
+    app_proc.wait()
+    print("App process exited.")
+
+    launcher_proc.wait()
+    print("Launcher process exited.")
 
 
 if __name__ == "__main__":
